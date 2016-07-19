afcbff325ba4644393e5ce7ac2126669fcd3247d
Adam Pine <apine@PC67827.corporate.gannettfleming.com>
2016-07-19 16:17:48 -0400

    fixes issue where pressing enter on insert was causing insert to insert for each and every row.

--- src/fields/jsgrid.field.sum.js
+++ src/fields/jsgrid.field.sum.js
@@ -20,34 +20,6 @@
                 sum += item[value];
             })
             return sum;
-        },
-        filterValue: function () {
-            var sum = 0;
-            $.each(this.columns, function (field, value) {
-                sum += item[value];
-            })
-            return sum;
-        },
-
-        insertValue: function () {
-            var sum = 0;
-            $.each(this.columns, function (field, value) {
-                sum += item[value];
-            })
-            return sum;
-        },
-
-        editValue: function () {
-            var sum = 0;
-            $.each(this.columns, function (field, value) {
-                sum += item[value];
-            })
-            return sum;
-        },
-
-        _createTextBox: function () {
-            return $("<input>").attr("type", "number")
-                .prop("readonly", !!this.readOnly);
         }
     });
 
